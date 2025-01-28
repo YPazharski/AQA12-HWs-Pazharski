2b8ca7b87d6b039210337d0d49e0e35892946f5c
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-01-28 18:49:37 +0300

    Delete unused elements

--- src/test/java/by/PazharskiYury/Lesson15/Task2.java
+++ src/test/java/by/PazharskiYury/Lesson15/Task2.java
@@ -4,14 +4,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.Set;
 
 public class Task2 {
 
-    private static final String[] URLS = new String[] {
+    private static final String[] URLS = new String[]{
             "http://www.automationpractice.pl/index.php",
             "https://zoo.waw.pl/",
             "https://www.w3schools.com/",
@@ -28,7 +26,6 @@ public class Task2 {
             driver.switchTo().newWindow(WindowType.WINDOW).get(URLS[i]);
         }
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         Set<String> windowHandles = driver.getWindowHandles();
         for (String windowHandle : windowHandles) {
             driver.switchTo().window(windowHandle);
