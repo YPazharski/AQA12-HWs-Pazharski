3b572b8d73e48fdcf40774bbf9f929d70f724d9b
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-02-12 21:17:58 +0300

    Delete test ng because of wrong failures, refactor of MainPageTests.java

--- src/test/java/by/PazharskiYury/MainPageTests.java
+++ src/test/java/by/PazharskiYury/MainPageTests.java
@@ -5,8 +5,7 @@ import by.PazharskiYury.Lesson18.MainPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.LocalDate;
 
@@ -17,36 +16,35 @@ public class MainPageTests {
     private static final String EMAIL = "okuh@mail.ru";
     private static final String PASSWORD = "qwerty12";
     private WebDriver driver;
-    private MainPage mainPage;
+    private MainPage.EditProfileForm editProfileForm;
 
     @BeforeClass
     public void setupDriver() {
         WebDriverManager.chromedriver().setup();
     }
 
-//    @BeforeMethod
-//    public void initNewDriver() {
-//        driver = new ChromeDriver();
-//    }
-//
-//    @BeforeMethod(dependsOnMethods = "initNewDriver")
-//    public void openMainPage() {
-//        mainPage = MainPage.open(driver, EMAIL, PASSWORD);
-//    }
+    @BeforeMethod
+    public void initializeDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().minimize();
+    }
 
-//    @AfterMethod
-//    public void quitDriver() {
-//        driver.quit();
-//    }
+    @BeforeMethod(dependsOnMethods = "initializeDriver")
+    public void initializeEditProfileForm() {
+        editProfileForm = MainPage
+                .open(driver, EMAIL, PASSWORD)
+                .clickEditProfileButton();
+    }
+
+    @AfterMethod
+    public void quitDriver() {
+        driver.quit();
+    }
 
     @Test
     public void maipe0() {
         String newName = "Name";
         String altNewName = "Noname";
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         String pastName = editProfileForm
                 .getFieldValue(EditProfileField.FIRST_NAME);
         String name = pastName.equals(newName) ? altNewName : newName;
@@ -54,7 +52,6 @@ public class MainPageTests {
                 .writeFieldValue(EditProfileField.FIRST_NAME, name)
                 .clickSave()
                 .getProfileFirstName();
-        driver.quit();
         assertEquals(displayingName, name);
     }
 
@@ -62,10 +59,6 @@ public class MainPageTests {
     public void maipe1() {
         String newName = "Same";
         String altNewName = "Nosame";
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         String pastName = editProfileForm
                 .getFieldValue(EditProfileField.LAST_NAME);
         String name = pastName.equals(newName) ? altNewName : newName;
@@ -73,7 +66,6 @@ public class MainPageTests {
                 .writeFieldValue(EditProfileField.LAST_NAME, name)
                 .clickSave()
                 .getProfileLastName();
-        driver.quit();
         assertEquals(displayingName, name);
     }
 
@@ -81,17 +73,12 @@ public class MainPageTests {
     public void maipe2() {
         LocalDate newDate = LocalDate.of(1996, 12, 20);
         LocalDate altNewDate = LocalDate.of(1997, 6, 15);
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         LocalDate pastDate = editProfileForm.getDateOfBirth();
         LocalDate date = pastDate.equals(newDate) ? altNewDate : newDate;
         LocalDate displayingDate = editProfileForm
                 .writeDateOfBirth(date)
                 .clickSave()
                 .getProfileDateOfBirth();
-        driver.quit();
         assertEquals(displayingDate, date);
     }
 
@@ -99,17 +86,12 @@ public class MainPageTests {
     public void maipe3() {
         LocalDate newDate = LocalDate.of(1996, 12, 20);
         LocalDate altNewDate = LocalDate.of(1997, 6, 15);
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         LocalDate pastDate = editProfileForm.getDateOfBirth();
         LocalDate date = pastDate.equals(newDate) ? altNewDate : newDate;
         LocalDate displayingDate = editProfileForm
                 .selectDateOfBirth(date)
                 .clickSave()
                 .getProfileDateOfBirth();
-        driver.quit();
         assertEquals(displayingDate, date);
     }
 
@@ -117,10 +99,6 @@ public class MainPageTests {
     public void maipe4() {
         String newName = "Name";
         String altNewName = "Noname";
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         String pastName = editProfileForm
                 .getFieldValue(EditProfileField.FIRST_NAME);
         String name = pastName.equals(newName) ? altNewName : newName;
@@ -129,7 +107,6 @@ public class MainPageTests {
                 .clickCancel()
                 .clickEditProfileButton()
                 .getFieldValue(EditProfileField.FIRST_NAME);
-        driver.quit();
         assertEquals(displayingName, pastName);
     }
 
@@ -137,10 +114,6 @@ public class MainPageTests {
     public void maipe5() {
         String newName = "Same";
         String altNewName = "Nosame";
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         String pastName = editProfileForm
                 .getFieldValue(EditProfileField.LAST_NAME);
         String name = pastName.equals(newName) ? altNewName : newName;
@@ -149,7 +122,6 @@ public class MainPageTests {
                 .clickCancel()
                 .clickEditProfileButton()
                 .getFieldValue(EditProfileField.LAST_NAME);
-        driver.quit();
         assertEquals(displayingName, pastName);
     }
 
@@ -157,10 +129,6 @@ public class MainPageTests {
     public void maipe6() {
         LocalDate newDate = LocalDate.of(1996, 12, 20);
         LocalDate altNewDate = LocalDate.of(1997, 6, 15);
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         LocalDate pastDate = editProfileForm.getDateOfBirth();
         LocalDate date = pastDate.equals(newDate) ? altNewDate : newDate;
         LocalDate displayingDate = editProfileForm
@@ -168,7 +136,6 @@ public class MainPageTests {
                 .clickCancel()
                 .clickEditProfileButton()
                 .getDateOfBirth();
-        driver.quit();
         assertEquals(displayingDate, pastDate);
     }
 
@@ -176,10 +143,6 @@ public class MainPageTests {
     public void maipe7() {
         String newName = "Name";
         String altNewName = "Noname";
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         String pastName = editProfileForm
                 .getFieldValue(EditProfileField.FIRST_NAME);
         String name = pastName.equals(newName) ? altNewName : newName;
@@ -188,7 +151,6 @@ public class MainPageTests {
                 .clickClose()
                 .clickEditProfileButton()
                 .getFieldValue(EditProfileField.FIRST_NAME);
-        driver.quit();
         assertEquals(displayingName, pastName);
     }
 
@@ -196,10 +158,6 @@ public class MainPageTests {
     public void maipe8() {
         String newName = "Same";
         String altNewName = "Nosame";
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         String pastName = editProfileForm
                 .getFieldValue(EditProfileField.LAST_NAME);
         String name = pastName.equals(newName) ? altNewName : newName;
@@ -208,7 +166,6 @@ public class MainPageTests {
                 .clickClose()
                 .clickEditProfileButton()
                 .getFieldValue(EditProfileField.LAST_NAME);
-        driver.quit();
         assertEquals(displayingName, pastName);
     }
 
@@ -216,10 +173,6 @@ public class MainPageTests {
     public void maipe9() {
         LocalDate newDate = LocalDate.of(1996, 12, 20);
         LocalDate altNewDate = LocalDate.of(1997, 6, 15);
-        WebDriver driver = new ChromeDriver();
-        var editProfileForm = MainPage
-                .open(driver, EMAIL, PASSWORD)
-                .clickEditProfileButton();
         LocalDate pastDate = editProfileForm.getDateOfBirth();
         LocalDate date = pastDate.equals(newDate) ? altNewDate : newDate;
         LocalDate displayingDate = editProfileForm
@@ -227,7 +180,6 @@ public class MainPageTests {
                 .clickClose()
                 .clickEditProfileButton()
                 .getDateOfBirth();
-        driver.quit();
         assertEquals(displayingDate, pastDate);
     }
 
