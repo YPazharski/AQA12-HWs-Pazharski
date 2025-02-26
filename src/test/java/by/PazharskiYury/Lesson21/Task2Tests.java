9c79f31d5aaaee5d24cedc6550fa46f9a457c040
Yury Pazharski <Pozharskiyuri@gmail.com>
2025-02-26 15:08:12 +0300

    Task 2 refactor

--- src/test/java/by/PazharskiYury/Lesson21/Task2Tests.java
+++ src/test/java/by/PazharskiYury/Lesson21/Task2Tests.java
@@ -1,67 +1,49 @@
 package by.PazharskiYury.Lesson21;
 
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
+import org.testng.annotations.*;
 import java.time.*;
 
 import static org.testng.Assert.assertEquals;
 
 public class Task2Tests {
 
-    private static final int EXPECTED_ELEMENTS_COUNT = 42;
     private static final LocalDateTime EXPECTED_DATE_TIME = LocalDateTime
             .now(Clock.tickMinutes(ZoneId.systemDefault()))
             .plusDays(1)
             .withHour(23)
             .withMinute(11);
 
-    private AndroidDriver androidDriver;
-    private ApiDemosMainPage.ApiDemosViewsPage viewsPage;
     private ApiDemosMainPage.ApiDemosViewsPage.DateWidgetsPage.DialogPage dialogPage;
 
-
-    @BeforeMethod
-    private void setup() {
+    @BeforeClass
+    public void setup() {
         AndroidDriverManager.setup(AndroidDriverManager.AndroidVirtualDevice.ELEMENT_COUNTER_DEVICE);
-        androidDriver = AndroidDriverManager.getAndroidDriver();
-        viewsPage = new ApiDemosMainPage(androidDriver).tapViews();
+        AndroidDriver androidDriver = AndroidDriverManager.getAndroidDriver();
+        dialogPage = new ApiDemosMainPage(androidDriver)
+                .tapViews()
+                .tapDateWidgets()
+                .tapDialog();
     }
 
-    @AfterMethod
-    private void tearDown() {
+    @AfterClass
+    public void tearDown() {
         AndroidDriverManager.quitDriver();
     }
 
     @Test
-    private void allMenuElementsDisplayed() {
-        WebElement lastElement = viewsPage.getDisplayedMenuElements().getLast();
-        TouchControls testFinger = new TouchControls("Test Finger", androidDriver);
-        testFinger.swipeUp(lastElement, 3000);
-        WebElement afterSwipeLastElement = viewsPage.getDisplayedMenuElements().getLast();
-        assertEquals(afterSwipeLastElement.getLocation(), lastElement.getLocation());
-        assertEquals(afterSwipeLastElement.getText(), lastElement.getText());
-        assertEquals(viewsPage.getDisplayedMenuElements().size(), EXPECTED_ELEMENTS_COUNT);
-    }
-
-    @Test
-    private void changedDateIsDisplayed() {
+    public void changedDateIsDisplayed() {
         LocalDate expectedDate = EXPECTED_DATE_TIME.toLocalDate();
-        dialogPage = viewsPage
-                .tapDateWidgets()
-                .tapDialog()
+        dialogPage
                 .tapChangeTheDate()
                 .chooseDate(expectedDate)
                 .tapOk();
         assertEquals(dialogPage.getDisplayedDateTime().toLocalDate(), expectedDate);
     }
 
-    @Test(priority = 4)
-    private void changedTimeIsDisplayed() {
-        LocalTime timeToSet = LocalTime.of(23, 11);
+    @Test
+    public void changedTimeIsDisplayed() {
+        LocalTime timeToSet = EXPECTED_DATE_TIME.toLocalTime();
         dialogPage
                 .tapChangeTheTimeWithSpinner()
                 .chooseTime(timeToSet)
@@ -69,36 +51,15 @@ public class Task2Tests {
         assertEquals(dialogPage.getDisplayedDateTime().toLocalTime(), timeToSet);
     }
 
-    @Test(priority = 5)
-    private void changedDateTimeIsDisplayed() {
+    @Test(dependsOnMethods = {"changedDateIsDisplayed", "changedTimeIsDisplayed"})
+    public void changedDateTimeIsDisplayed() {
         assertEquals(dialogPage.getDisplayedDateTime(), EXPECTED_DATE_TIME);
     }
 
-    @Test(priority = 6)
-    private void changedDateTimeIsSaved() {
+    @Test(dependsOnMethods = "changedDateTimeIsDisplayed")
+    public void changedDateTimeIsSaved() {
         LocalDateTime displayedDateTime = dialogPage.backToDateWidgetsPage().tapDialog().getDisplayedDateTime();
         assertEquals(displayedDateTime, EXPECTED_DATE_TIME);
-    }
-
-    @Test(priority = 7)
-    private void textSwitcherNextButtonIncreasesDisplayedNumberByOne() {
-        ApiDemosMainPage.ApiDemosViewsPage.TextSwitcherPage textSwitcherPage =
-                dialogPage.backToDateWidgetsPage().backToViewsPage().tapTextSwitcher();
-        int initialValue = textSwitcherPage.getDisplayedNumber();
-        int fibbo = 0;
-        int nacci = 1;
-        int tapsCount;
-        for (int i = 0; i < 6; i++) {
-            tapsCount = fibbo + nacci;
-            for (int j = tapsCount; j > 0; j--) {
-                textSwitcherPage.tapNext();
-            }
-            fibbo = nacci;
-            nacci = tapsCount;
-            int displayedNumber = textSwitcherPage.getDisplayedNumber();
-            assertEquals(displayedNumber, initialValue + tapsCount);
-            initialValue = displayedNumber;
-        }
     }
 
 }
